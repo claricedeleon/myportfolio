d0e6a4e67c17430749dbd928b667df2744f68ee7
claricedeleon <juneclaricedeleon@gmail.com>
2021-06-26 12:13:22 +0800

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
@@ -8,14 +8,12 @@ import Resume from './components/Resume';
 function App() {
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about-me" exact component={AboutMe} />
-          <Route path="/resume" exact component={Resume} />
-          <Route path="/portfolio" exact component={Portfolio} />
-        </Switch>
-      </Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about-me" exact component={AboutMe} />
+        <Route path="/resume" exact component={Resume} />
+        <Route path="/portfolio" exact component={Portfolio} />
+      </Switch>
     </div>
   );
 }
