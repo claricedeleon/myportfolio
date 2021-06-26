3363ba904291efcf624c51d2db6cdf2e877878ad
claricedeleon <juneclaricedeleon@gmail.com>
2021-06-26 12:52:23 +0800

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
@@ -8,12 +8,14 @@ import Resume from './components/Resume';
 function App() {
   return (
     <div>
-      <Switch>
-        <Route path="#" exact component={Home} />
-        <Route path="about-me" component={AboutMe} />
-        <Route path="resume" component={Resume} />
-        <Route path="portfolio" component={Portfolio} />
-      </Switch>
+      <HashRouter basename="/myportfolio">
+        <Switch>
+          <Route path="/#" exact component={Home} />
+          <Route path="/about-me" component={AboutMe} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/portfolio" component={Portfolio} />
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
