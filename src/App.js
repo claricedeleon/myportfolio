59bd5f0a1e865d11aec7226e768570da7632e41a
claricedeleon <juneclaricedeleon@gmail.com>
2021-06-26 12:41:18 +0800

    made some chages

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div>
       <Switch>
         <Route path="/#" exact component={Home} />
-        <Route path="/about-me" component={AboutMe} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/myportfolio/about-me" component={AboutMe} />
+        <Route path="/myportfolio/resume" component={Resume} />
+        <Route path="/myportfolio/portfolio" component={Portfolio} />
       </Switch>
     </div>
   );
